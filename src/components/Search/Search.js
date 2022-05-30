98fd2d2d9fb8ca57d06de6f5d22ea20a663d7373
Raunak173 <raunakagarwal173@gmail.com>
2022-05-30 14:28:40 +0530

    made landing page

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,13 +4,49 @@ import "./Search.css";
 import hi from "../../images/hi.svg";
 import hi1 from "../../images/hi1.svg";
 import loc from "../../images/loc.svg";
+import cal from "../../images/cal.svg";
+import arr from "../../images/arr.svg";
 
 const Search = () => {
   return (
     <div className="mt-16 flex flex-col gap-y-11">
       <p className="text-center heading">Where are you going?</p>
-      <div className="search-box m-auto flex items-center px-12">
-        <img src={loc} width={15.6} height={19.5} />
+      <div className="search-box m-auto flex items-center px-12 gap-x-10">
+        <div className="flex gap-x-4">
+          <img src={loc} width={15.6} height={19.5} />
+          <input
+            placeholder="Dubai, United Arab Emirates"
+            className="w-[650px]"
+          />
+        </div>
+        <img src={cal} />
+        <div className="flex">
+          <div className="flex flex-col gap-y-1 w-[90px]">
+            <p className="c">Check-in</p>
+            <input
+              type="text"
+              placeholder="15 May 2022"
+              onfocus="(this.type='date')"
+            />
+          </div>
+          <p className="c1 ml-8">to</p>
+          <div className="flex flex-col gap-y-1 ml-8 w-[90px]">
+            <p className="c">Check-out</p>
+            <input
+              type="text"
+              placeholder="20 May 2022"
+              onfocus="(this.type='date')"
+            />
+          </div>
+        </div>
+        <div className="flex gap-x-2">
+          <p className="c2 w-[50px]">1 Room</p>
+          <img src={arr} />
+        </div>
+        <div className="flex gap-x-2">
+          <p className="c2 w-[56px]">2 Adults</p>
+          <img src={arr} />
+        </div>
       </div>
       <button className="btn m-auto">Search Hotels</button>
       <div
